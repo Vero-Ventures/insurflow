ae0144d775ef2238fbd1bf3665518a835a52ace3
Matheus Demeis <mfariasdemeis@my.bcit.ca>
2026-01-27 13:41:25 -0800

    chore: cleanup providers and layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,6 @@ export default function RootLayout({
       suppressHydrationWarning
     >
       <body>
-        <Providers>{children}</Providers>
         <Providers>
           <AppNavigationMenu />
           {children}
