4d3f95716c50b499c44a52ac7428cfe86eff4cf6
Matheus Demeis <mfariasdemeis@my.bcit.ca>
2026-02-19 23:14:05 -0800

    fix(api-call): send empty JSON body to calculate endpoint to prevent 400 invalid JSON

--- src/lib/hooks/use-insurance-needs.ts
+++ src/lib/hooks/use-insurance-needs.ts
@@ -107,6 +107,7 @@ export function useInsuranceNeeds(
         headers: {
           "Content-Type": "application/json",
         },
+        body: JSON.stringify({}),
         credentials: "include",
       });
 
