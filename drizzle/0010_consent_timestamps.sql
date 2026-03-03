c563ccd9e3b19648aadf9559d023aa29077d02c3
Matheus Demeis <mfariasdemeis@my.bcit.ca>
2026-03-02 21:22:44 -0800

    fix: make index drop migration idempotent

--- drizzle/0010_consent_timestamps.sql
+++ drizzle/0010_consent_timestamps.sql
@@ -1,4 +1,4 @@
-DROP INDEX "d2c_resume_link_token_idx";--> statement-breakpoint
+DROP INDEX IF EXISTS "d2c_resume_link_token_idx";--> statement-breakpoint
 ALTER TABLE "client" ADD COLUMN "consent_transmit_to_carrier_at" timestamp with time zone;--> statement-breakpoint
 ALTER TABLE "client" ADD COLUMN "health_info_authorization_at" timestamp with time zone;--> statement-breakpoint
 ALTER TABLE "client" ADD COLUMN "esign_intent_acknowledged_at" timestamp with time zone;
