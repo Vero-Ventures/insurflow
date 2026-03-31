6a4e92d6d12b96a33da1c3ef9509dfb57267fa8f
Lam Thai <tuoclamthai@gmail.com>
2026-03-31 14:02:52 -0700

    fix: fix for deployment

--- src/env.js
+++ src/env.js
@@ -32,19 +32,10 @@ export const env = createEnv({
     BETTER_AUTH_GITHUB_CLIENT_SECRET: z.string().optional(),
     BETTER_AUTH_GOOGLE_CLIENT_ID: z.string().optional(),
     BETTER_AUTH_GOOGLE_CLIENT_SECRET: z.string().optional(),
-    GMAIL_SMTP_USER:
-      process.env.NODE_ENV === "production"
-        ? z.string()
-        : z.string().optional(),
-    GMAIL_APP_PASSWORD:
-      process.env.NODE_ENV === "production"
-        ? z.string()
-        : z.string().optional(),
+    GMAIL_SMTP_USER: z.string().optional(),
+    GMAIL_APP_PASSWORD: z.string().optional(),
     RESEND_API_KEY: z.string().optional(),
-    AUTH_EMAIL_FROM:
-      process.env.NODE_ENV === "production"
-        ? z.string()
-        : z.string().optional(),
+    AUTH_EMAIL_FROM: z.string().optional(),
     DATABASE_URL: z.string().url(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
